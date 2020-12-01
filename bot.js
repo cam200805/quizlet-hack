3d10cb907d9c6daa7d74561de40862cce6136a03
Pandapip1 <45835846+Pandapip1@users.noreply.github.com>
2020-12-01 14:14:17 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,43 @@
-var map = {};
-var toggle = false;
-document.getElementsByClassName("FormattedTextWithImage").forEach((e) => {
-	if (toggle){
-	    map[toggle] = e.innerText;
-	    toggle = false;
-	} else {
-	    toggle = e.innerText;
+function qb_canShiftLeft(){}
+function qb_canShiftRight(){}
+function qb_shiftLeft(){}
+function qb_shiftRight(){}
+
+function qb_generateSetOfAnswerPairs(){
+	var map = {};
+	var toggle = false;
+	document.getElementsByClassName("FormattedTextWithImage").forEach((e) => {
+		if (toggle){
+			map[toggle] = e.innerText;
+			toggle = false;
+		} else {
+			toggle = e.innerText;
+		}
+	})
+	return map;
+}
+function qb_generateAllAnswerPairs(){
+	while (qb_canShiftLeft()){
+		qb_shiftLeft();
 	}
-})
-document.getElementsByClassName("NewStudentAnswerOptions").forEach((e) => {})
+	qb_shiftFarLeft();
+	var finalMap = {};
+	while (qb_canShiftRight()){
+		var map = qb_generateSetOfAnswerPairs();
+		for (var key in map){
+			finalMap[key] = map;
+		}
+		qb_shiftRight();
+	}
+	return finalMap;
+}
+function qb_mainLoop(answers){
+	return () => {
+		if (!qb_answerElementExists()){
+			return;
+		}
+		qb_selectCorrectAnswer(answers);
+		document.getElementsByClassName("NewStudentAnswerOptions").forEach((e) => {})
+	}
+}
+setInterval(qb_mainLoop(qb_generateAllAnswerPairs()), 1000);
