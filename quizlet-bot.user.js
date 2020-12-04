e8baac786eaf8117e677d69dcc7377ea8be6f328
Pandapip1 <45835846+Pandapip1@users.noreply.github.com>
2020-12-04 15:13:04 -0500

    Update quizlet-bot.user.js

--- quizlet-bot.user.js
+++ quizlet-bot.user.js
@@ -10,23 +10,15 @@
 // @grant        none
 // ==/UserScript==
 (() => {
-var qb_userTriggered = false;
-
-var qb_script_jquery = document.createElement('script');
-var qb_script_bot = document.createElement('script');
-
-qb_script_jquery.src = "https://pandapip1.github.io/quizlet-bot/bot.js";
-qb_script_bot.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js";
-
-document.head.appendChild(qb_script_jquery);
-qb_script_jquery.onload = () => {
-    $("button").click((() => {
+    var qb_userTriggered = false;
+    var qb_script_bot = document.createElement('script');
+    qb_script_bot.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js";
+    $("button").click(() => {
         if (!qb_userTriggered){
             qb_userTriggered = true;
             setTimeout(() => {
                 document.head.appendChild(qb_script_bot);
             }, 1000); // give it time to load
         }
-    }));
-}
+    });
 })();
