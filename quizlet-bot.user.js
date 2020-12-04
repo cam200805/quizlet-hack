43296d5f9eef2708930239c07a41ce1560d05924
Pandapip1 <45835846+Pandapip1@users.noreply.github.com>
2020-12-04 15:04:37 -0500

    Update quizlet-bot.user.js

--- quizlet-bot.user.js
+++ quizlet-bot.user.js
@@ -10,13 +10,21 @@
 // @grant        none
 // ==/UserScript==
 var qb_userTriggered = false;
-$("button").click((() => {
-    if (!qb_userTriggered){
-        qb_userTriggered = true;
-        setTimeout(() => {
-            var script = document.createElement('script');
-            script.src = "https://cdn.jsdelivr.net/gh/pandapip1/quizlet-bot/bot.js";
-            document.head.appendChild(script);
-        }, 1000); // give it time to load
-    }
-}));
+
+var qb_script_jquery = document.createElement('script');
+var qb_script_bot = document.createElement('script');
+
+qb_script_jquery.src = "https://cdn.jsdelivr.net/gh/pandapip1/quizlet-bot/bot.js";
+qb_script_bot.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js";
+
+document.head.appendChild(qb_script_jquery);
+qb_script_jquery.onload = () => {
+    $("button").click((() => {
+        if (!qb_userTriggered){
+            qb_userTriggered = true;
+            setTimeout(() => {
+                document.head.appendChild(qb_script_bot);
+            }, 1000); // give it time to load
+        }
+    }));
+}
