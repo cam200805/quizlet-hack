f9fa12e9599b815e5cf5660df84e8156a941e289
Pandapip1 <45835846+Pandapip1@users.noreply.github.com>
2020-12-04 15:07:14 -0500

    Update quizlet-bot.user.js

--- quizlet-bot.user.js
+++ quizlet-bot.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Quizlet bot
 // @namespace    https://pandapip1.tk/
-// @version      0.1
+// @version      0.1.1
 // @description  A bot for Quizlet Live
 // @author       Pandapip1
 // @match        https://quizlet.com/live
