7c9a585d698d14ea922dfc136517b52a9eccd5b0
Pandapip1 <45835846+Pandapip1@users.noreply.github.com>
2021-01-15 12:52:01 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,30 +1,35 @@
+function qb_sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 function qb_canShiftLeft(){
-	return document.getElementsByClassName("UIIcon--arrow-left").length != 0;
+	return !$(".previousButton span button").disabled
 }
 function qb_canShiftRight(){
-	return document.getElementsByClassName("UIIcon--arrow-right").length != 0;
+	return !$(".previousButton span button").disabled
 }
 function qb_shiftLeft(){
-	document.getElementsByClassName("UIIcon--arrow-left")[0].click();
+	$(".previousButton span button").click()
 }
 function qb_shiftRight(){
-	document.getElementsByClassName("UIIcon--arrow-right")[0].click();
+	$(".nextButton span button").click()
 }
 
 function qb_getCorrectAnswer(answers){
 	return answers[document.getElementsByClassName("StudentPrompt-text")[0].innerText];
 }
 
-function qb_generateSetOfAnswerPairs(){
+async function qb_generateSetOfAnswerPairs(){
 	var map = {};
 	var toggle = false;
-	document.getElementsByClassName("StudentAnswerOption-text").forEach((e) => {
+	document.getElementsByClassName("FormattedTextWithImage").forEach((e) => {
 		if (toggle){
 			map[toggle] = e.innerText;
 			toggle = false;
 		} else {
 			toggle = e.innerText;
 		}
+		await qb_sleep(1000);
 	})
 	return map;
 }
@@ -61,7 +66,7 @@ function qb_mainLoop(answers){return () => {
 	qb_selectCorrectAnswer(answer);
 }}
 if (window.location.hostname == "quizlet.com"){
-	setInterval(qb_mainLoop(qb_generateAllAnswerPairs()), 1000);
+	(async () => {setInterval(await qb_mainLoop(qb_generateAllAnswerPairs()), 1000);})()
 } else if (window.location.hostname == "pandapip1.github.io"){
 	alert("Drag the link to the bookmarks bar to create the bookmarklet."); // The user clicked on the bookmarklet creation link instead of dragging it
 }
